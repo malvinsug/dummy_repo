11b1eb4bc706e37b93e53e206369c8ba9908e4ae
Malvin Yurie Sugiri <malvin.sugiri@yahoo.com>
2020-09-23 13:05:39 +0200

    try to get author name 3  + some docs

--- src/main/java/api/UpdateSheet.java
+++ src/main/java/api/UpdateSheet.java
@@ -17,14 +17,45 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
+/**
+ * This class will be excecuted once the programmer pushes his/her codes into Git. The
+ * continuous integration by Travis will be automatically triggered and this program
+ * will be excecuted after the machine performs tests.
+ *
+ * The program will update the test report to Google spreadsheet automatically using
+ * Google API. The test report consist of first name of the author, last name of the
+ * author and also the test result (either successful or fail).
+ */
 public class UpdateSheet {
   private static Sheets sheetsService;
   private static String APPLICATION_NAME = "DummyApp";
   private static String SPREADSHEET_ID = "1BoXHu683YywbVb400FOS5Emn3GSOr-VUH54fmwhk6Jo";
 
+  private static final int RELEVANT_ARGUMENT_LENGTH = 3;
+  private static final int FIRST_NAME_INDEX = 0;
+  private static final int LAST_NAME_INDEX = 1;
+  private static final int RESULT_INDEX = 2;
+
+  /**
+   * The main program access the test report spreadsheet file from Google Spreadsheet
+   * and it updates the report.
+   * @param args args is passed arguments from yml-file.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+  public static void main(String[] args) throws IOException, GeneralSecurityException {
+    sheetsService = getSheetsService();
+    args = fixArgs(args);
+    writeSpreadsheet(args);
+  }
+
+  /**
+   * This method authorizes the program before it accesses a spreadsheet file.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
   private static Credential authorize() throws IOException, GeneralSecurityException {
     InputStream in = UpdateSheet.class.getResourceAsStream("/credentials.json");
     GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
@@ -46,7 +77,12 @@ public class UpdateSheet {
     return credential;
   }
 
-  public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+  /**
+   * This method access the needed spreadsheet from Google spreadsheet.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+  private static Sheets getSheetsService() throws IOException, GeneralSecurityException {
     Credential credential = authorize();
     return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
             JacksonFactory.getDefaultInstance(), credential)
@@ -54,43 +90,60 @@ public class UpdateSheet {
             .build();
   }
 
-  public static void main(String[] args) throws IOException, GeneralSecurityException {
-    sheetsService = getSheetsService();
-    //readSpreadsheet();
-    writeSpreadsheet(args);
-  }
-
-  private static void readSpreadsheet() throws IOException {
-    String range = "Test_Result!A1:C1";
-
-    ValueRange response = sheetsService.spreadsheets().values()
-            .get(SPREADSHEET_ID,range)
-            .execute();
-
-    List<List<Object>> values = response.getValues();
-
-    if (values == null || values.isEmpty()) {
-      System.out.println("No data found");
+  /**
+   * This method will extract the necessary data for test report. The relevant
+   * data should be the first name of the author, last name of the author, and
+   * the test result (either successful or fail).
+   * @param args is passed arguments from yml-file.
+   * @return relevant data for test report.
+   */
+  private static String[] fixArgs(String[] args) {
+    if (hasLastNameOnly(args)) {
+      args = addEmptyFirstName(args);
     } else {
-      for (List row : values) {
-        System.out.printf("%s, %s, %s\n", row.get(2),row.get(1),row.get(0));
-      }
+      args = getFirstAndLastNameOnly(args);
     }
-
-
+    return args;
   }
 
+  /**
+   * This method writes the necessary data into the test report in
+   * Google spreadsheet.
+   * @param args is the necessary data that need to be written in
+   *             the test report.
+   * @throws IOException
+   */
   private static void writeSpreadsheet(String[] args) throws IOException {
     ValueRange appendBody = new ValueRange()
             .setValues(Arrays.asList(
                     Arrays.asList(args[0],args[1],args[2])
             ));
 
-    AppendValuesResponse appendResult = sheetsService.spreadsheets().values()
+    sheetsService.spreadsheets().values()
             .append(SPREADSHEET_ID,"report", appendBody)
             .setValueInputOption("USER_ENTERED")
             .setInsertDataOption("INSERT_ROWS")
             .setIncludeValuesInResponse(true)
             .execute();
+  }
+
+  private static String[] addEmptyFirstName(String[] args) {
+    String[] fixedArgs = new String[RELEVANT_ARGUMENT_LENGTH];
+    fixedArgs[FIRST_NAME_INDEX] = "";
+    fixedArgs[LAST_NAME_INDEX] = args[0];
+    fixedArgs[RESULT_INDEX] = args[args.length-1];
+    return fixedArgs;
+  }
+
+  private static String[] getFirstAndLastNameOnly(String[] args) {
+    String[] fixedArgs = new String[RELEVANT_ARGUMENT_LENGTH];
+    fixedArgs[FIRST_NAME_INDEX] = args[0];
+    fixedArgs[LAST_NAME_INDEX] = args[args.length-2];
+    fixedArgs[RESULT_INDEX] = args[args.length-1];
+    return fixedArgs;
+  }
+
+  private static boolean hasLastNameOnly(String[] args) {
+    return args.length == 2;
   }
 }
--- src/main/java/crewmeister/CrewmeisterChallenge.java
+++ src/main/java/crewmeister/CrewmeisterChallenge.java
@@ -1,18 +1,23 @@
 package crewmeister;
 
 public class CrewmeisterChallenge {
+  //TODO: implement crewmeisterRevert
   /**
    * A method to reverse a string. For example, if the input is 'abc', then it should return 'cba'.
    * @param word is a string that has to be reversed.
    * @return a reversed string.
    */
   public static String crewmeisterRevert(String word) {
-    char[] wordElements = word.toCharArray();
-    StringBuilder reversedWord = new StringBuilder();
-    for (int index = wordElements.length - 1;index >= 0;index--) {
-      reversedWord.append(wordElements[index]);
-    }
-    return reversedWord.toString();
+    // This is an implementation that I would have done.
+    /* char[] wordElements = word.toCharArray();
+     * StringBuilder reversedWord = new StringBuilder();
+     * for (int index = wordElements.length - 1;index >= 0;index--) {
+     *   reversedWord.append(wordElements[index]);
+     * }
+     * return reversedWord.toString();
+     */
+    String reversedWord = "";
+    return reversedWord;
   }
 
 }
